96fbccd1169f05d78eb0f4fb637ce33e099588cd
Matthew Davis <matthewdavis1372@yahoo.com>
2022-05-07 09:13:00 -0400

    updated colors of blocks to correspond with current hour

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// getting the date at the top out of the way lol
+
 var currentDay = $("#currentDay");
 var currentDayMoment = moment().format("dddd, MMMM Do");
 currentDay.text(currentDayMoment)
+
+// hour variable and other variables for time slots to change colors
+
+var currentHour = moment().hour();
+
+var hourSlots = $(".timeSlot").toArray();
+
+console.log(hourSlots)
+
+
+for (var i = 0; i < hourSlots.length; i++) {
+    var hourPlus = (i+9);
+    console.log(hourSlots[i])
+    if (hourPlus > currentHour){
+        hourSlots[i].classList.add("future");
+    } else if (hourPlus == currentHour) {
+        hourSlots[i].classList.add("present")
+    } else {
+        hourSlots[i].classList.add("past")
+    }
+}
