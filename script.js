1f5e47655db6a5e2851f9f4e6ce013ba703984db
Matthew Davis <matthewdavis1372@yahoo.com>
2022-05-07 11:20:28 -0400

    final commit with screen shot and comments to make it more clear what intentions are in script file

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 // getting the date at the top out of the way lol
+// these functions and the text update will show current date in the header of the page on loading
 
 var currentDay = $("#currentDay");
 var currentDayMoment = moment().format("dddd, MMMM Do");
 currentDay.text(currentDayMoment)
 
-// hour variable and other variables for time slots to change colors
+// hour variable and other variables for time slots to change colors. also array variable for the for loop later on
 var saveButton = $(".saveBtn");
 
 var currentHour = moment().hour();
 
 var hourSlots = $(".timeSlot").toArray();
 
-var saveButtonGroup = $(".saveBtn").toArray();
 
 
 
-
+//this for loop goes through array of all timeslots and checks to see if its past present or future hour, then changes class
+// of the timeslot to change the color accordingly
 for (var i = 0; i < hourSlots.length; i++) {
     var hourPlus = (i+9);
     if (hourPlus > currentHour){
@@ -27,6 +28,8 @@ for (var i = 0; i < hourSlots.length; i++) {
     }
 }
 
+
+// this function renders the local storage text into timeslots on loading the page
 function renderTasks(){
     for (var i = 0; i < hourSlots.length; i++){
         var checkid = hourSlots[i].id;
@@ -38,7 +41,7 @@ function renderTasks(){
     }
 }
 
-
+//this function is for saving the text in the timeslots
 function saveTask(event){
     event.preventDefault();
     var x = event.target.parentElement; //this is the form
@@ -50,6 +53,8 @@ function saveTask(event){
     localStorage.setItem(name, y)
 }
 
+
+// event listener for submit button that calls on function that saves text
 saveButton.on("click", saveTask);
 
 renderTasks();
