fc7028e3e7af667a4dc46437cc0f25f0bb41c007
Matthew Davis <matthewdavis1372@yahoo.com>
2022-05-07 10:56:06 -0400

    added the updating system for local storage and it also names the storage based on parent of event target

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ var currentDayMoment = moment().format("dddd, MMMM Do");
 currentDay.text(currentDayMoment)
 
 // hour variable and other variables for time slots to change colors
+var saveButton = $(".saveBtn");
 
 var currentHour = moment().hour();
 
 var hourSlots = $(".timeSlot").toArray();
 
-console.log(hourSlots)
+var saveButtonGroup = $(".saveBtn").toArray();
+
+
 
 
 for (var i = 0; i < hourSlots.length; i++) {
     var hourPlus = (i+9);
-    console.log(hourSlots[i])
     if (hourPlus > currentHour){
         hourSlots[i].classList.add("future");
     } else if (hourPlus == currentHour) {
@@ -24,3 +26,28 @@ for (var i = 0; i < hourSlots.length; i++) {
         hourSlots[i].classList.add("past")
     }
 }
+
+function renderTasks(){
+    for (var i = 0; i < hourSlots.length; i++){
+
+    }
+}
+
+
+function saveTask(event){
+    event.preventDefault();
+    var x = event.target.parentElement; //this is the form
+    console.log(x);
+    var y = x.children[1].value
+    console.log(y);
+    var name = x.children[1].id;
+    console.log(name)
+    localStorage.setItem(name, y)
+}
+
+saveButton.on("click", saveTask);
+
+
+
+
+
